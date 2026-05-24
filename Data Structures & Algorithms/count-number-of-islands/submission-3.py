4274f4d16fd1e5664ddbaa93f534ba5d3bb087b7
Shanyi Liow <shanyiliow1107@gmail.com>
2026-05-24 20:43:02 +0800

    Add: count-number-of-islands - submission-3

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-3.py
@@ -0,0 +1,37 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+        #up down left right
+        directions = [
+            [0, 1], [1, 0], [-1, 0], [0, -1]
+        ]
+        res = 0 #island count
+
+        def dfs(r, c):
+            #base case: invalid case
+            if (
+                r < 0 or c < 0 or r >= ROWS or c >= COLS or
+                (r, c) in visited or grid[r][c] == "0"
+            ):
+                return 
+            
+            #recursion idea: include backtrack exclude
+            #note: need to mark island as visited to avoid dup
+            visited.add((r, c))
+            grid[r][c] = "0"
+
+            #go thru 4 directions
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+            #note: no need remove from set bcs visited = history
+            #just update island to 0 to avoid revisit
+            # visited.remove((r, c))
+
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1": #only perform on island
+                    dfs(r, c)
+                    res += 1
+        return res
