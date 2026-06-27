13db532b55d35298bb4595b7a0628a3a35933be8
Shanyi Liow <shanyiliow1107@gmail.com>
2026-06-28 05:49:51 +0800

    Add: house-robber - submission-8

---
+++ Data Structures & Algorithms/house-robber/submission-8.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        #pattern: btm up approach
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(
+                dp[i - 1], #skip cur
+                dp[i - 2] + nums[i] #rob cur
+            )
+        return dp[-1]
+
