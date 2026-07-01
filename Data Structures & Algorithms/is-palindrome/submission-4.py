d4444512d3e7e1307d7054a7d78ef30fc5408dc0
Shanyi Liow <shanyiliow1107@gmail.com>
2026-07-02 00:22:45 +0800

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # remove all space and only keep alphanumeric chara
+        newStr = ""
+        for chara in s:
+            if chara.isalnum():
+                newStr += chara.lower()
+
+        l, r = 0, len(newStr) - 1
+        while l < r:
+            if newStr[l] != newStr[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
